fa53e322a0a029bb85e3b3c0442a486d4d05b2bb
Sam <flightunderwings@gmail.com>
2025-07-31 22:12:16 +0000

    on edit item page the author and author text box dont line up with publi

--- src/components/InventoryForm.tsx
+++ src/components/InventoryForm.tsx
@@ -277,7 +277,9 @@ const InventoryForm: React.FC<InventoryFormProps> = ({
         onDrop={handleDrop}
       >
         <DialogHeader className="px-6 pt-6 pb-2 border-b">
-          <DialogTitle className="truncate pr-8">{initialData ? `Edit Item: ${initialData.title}` : 'Add New Item'}</DialogTitle>
+          <DialogTitle className="truncate pr-8 flex items-baseline">
+            Edit Item: <span className="ml-2">{initialData ? initialData.title : ''}</span>
+          </DialogTitle>
            <Tabs value={`page${currentPage}`} onValueChange={handleTabChange} className="w-full pt-2">
             <TabsList className="grid w-full grid-cols-4">
               <TabsTrigger value="page1">Details</TabsTrigger>
